cf7e666ccd76d7bc89ec0aeccb6f0b9a084e4290
lucasmlg1 <lucasmlg@live.com>
2025-04-05 21:05:17 -0300

    Cor do título

--- src/Menu.java
+++ src/Menu.java
@@ -4,13 +4,17 @@ import java.util.Scanner;    // Importa a classe Scanner para capturar entradas 
 public class Menu {
     Player p1 = new Player(); // Cria um objeto Player para controlar o estado do jogador (vidas)
 
+    final String WHITE_BOLD_BRIGHT = "\033[1;97m";
+    public static final String RESET = "\033[0m";
+
+
     // Construtor da classe Menu (não faz nada nesse caso)
     public Menu() {
     }
 
     // Função para exibir o título do jogo usando caracteres ASCII
     void printTitle() {
-        String var1 = "-fff-------------------------------------------------------------------\nff----oooo--rr-rr----cccc---aa-aa-----gggggg---aa-aa-mm-mm-mmmm----eee-\nffff-oo--oo-rrr--r-cc------aa-aaa----gg---gg--aa-aaa-mmm--mm--mm-ee---e\nff---oo--oo-rr-----cc-----aa--aaa----ggggggg-aa--aaa-mmm--mm--mm-eeeee-\nff----oooo--rr------ccccc--aaa-aa---------gg--aaa-aa-mmm--mm--mm--eeeee\n--------------------------------------ggggg----------------------------\n";
+        String var1 = WHITE_BOLD_BRIGHT + "-fff-------------------------------------------------------------------\nff----oooo--rr-rr----cccc---aa-aa-----gggggg---aa-aa-mm-mm-mmmm----eee-\nffff-oo--oo-rrr--r-cc------aa-aaa----gg---gg--aa-aaa-mmm--mm--mm-ee---e\nff---oo--oo-rr-----cc-----aa--aaa----ggggggg-aa--aaa-mmm--mm--mm-eeeee-\nff----oooo--rr------ccccc--aaa-aa---------gg--aaa-aa-mmm--mm--mm--eeeee\n--------------------------------------ggggg----------------------------\n" + RESET;
         System.out.println(var1);  // Exibe o título estilizado
     }
 
