3a2268510b3542003a0bb5382b296d5276d35aa0
filipe-wanderley <filipemwanderley@gmail.com>
2025-04-07 14:18:28 -0300

    IntelliJ

--- src/Menu.java
+++ src/Menu.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;  // Importa a classe ArrayList para usar listas dinâmicas
 import java.util.Scanner;    // Importa a classe Scanner para capturar entradas do usuário
 
 public class Menu {
@@ -16,7 +15,14 @@ public class Menu {
 
     // Função para exibir o título do jogo usando caracteres ASCII
     void printTitle() {
-        String var1 = WHITE_BOLD_BRIGHT + "-fff-------------------------------------------------------------------\nff----oooo--rr-rr----cccc---aa-aa-----gggggg---aa-aa-mm-mm-mmmm----eee-\nffff-oo--oo-rrr--r-cc------aa-aaa----gg---gg--aa-aaa-mmm--mm--mm-ee---e\nff---oo--oo-rr-----cc-----aa--aaa----ggggggg-aa--aaa-mmm--mm--mm-eeeee-\nff----oooo--rr------ccccc--aaa-aa---------gg--aaa-aa-mmm--mm--mm--eeeee\n--------------------------------------ggggg----------------------------\n" + RESET;
+        String var1 = WHITE_BOLD_BRIGHT + """
+    ███████╗ ██████╗ ██████╗  ██████╗ █████╗      ██████╗  █████╗ ███╗   ███╗███████╗
+    ██╔════╝██╔═══██╗██╔══██╗██╔════╝██╔══██╗    ██╔════╝ ██╔══██╗████╗ ████║██╔════╝
+    █████╗  ██║   ██║██████╔╝██║     ███████║    ██║  ███╗███████║██╔████╔██║█████╗  
+    ██╔══╝  ██║   ██║██╔══██╗██║     ██╔══██║    ██║   ██║██╔══██║██║╚██╔╝██║██╔══╝  
+    ██║     ╚██████╔╝██║  ██║╚██████╗██║  ██║    ╚██████╔╝██║  ██║██║ ╚═╝ ██║███████╗
+    ╚═╝      ╚═════╝ ╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝     ╚═════╝ ╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝
+    """ + RESET;
         System.out.println(var1);  // Exibe o título estilizado
     }
 
@@ -26,7 +32,22 @@ public class Menu {
 
         do {
             System.out.println("Insira entre as opções abaixo.");
-            System.out.println(GREEN_BOLD_BRIGHT + "1-Jogar " + RED_BOLD_BRIGHT+ "2-Sair" + RESET);  // Opções disponíveis para o jogador
+            System.out.println(GREEN_BOLD_BRIGHT +
+                    " \n" +
+                    " _      __                         \n" +
+                    "/ |     \\ \\  ___   __ _  __ _ _ __ \n" +
+                    "| |      \\ \\/ _ \\ / _` |/ _` | '__|\n" +
+                    "| |_  /\\_/ / (_) | (_| | (_| | |   \n" +
+                    "|_(_) \\___/ \\___/ \\__, |\\__,_|_|   \n" +
+                    "                  |___/            \n" + RED_BOLD_BRIGHT+
+                    "  ___       _____       _      \n" +
+                    " |__ \\     / ____|     (_)     \n" +
+                    "    ) |   | (___   __ _ _ _ __ \n" +
+                    "   / /     \\___ \\ / _` | | '__|\n" +
+                    "  / /_ _   ____) | (_| | | |   \n" +
+                    " |____(_) |_____/ \\__,_|_|_|   \n" +
+                    "                               \n" +
+                    "                               " + RESET);  // Opções disponíveis para o jogador
             System.out.println("Sua opção:");
             var4 = var1.nextInt();  // Lê a escolha do jogador.
 
@@ -45,6 +66,7 @@ public class Menu {
         } while (var4 != 0);  // O laço continua até o jogador escolher uma opção válida
     }
 
+    // Função que inicia o jogo, controlando o fluxo do jogo e a interação com o jogador
     void initGame(Scanner scanner, Palavra palavra) {
         this.p1 = new Player();  // Reinicia o jogador
         char[] letrasTentadas = new char[26];  // Suporta até 26 letras diferentes
